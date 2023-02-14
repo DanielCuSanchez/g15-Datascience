ca0df4bfccfb30d0df3f674ad8e7672a045d33bc
DanielCu <danielcu@alternet.com.mx>
2023-02-13 21:27:26 -0600

    conditionals

---
+++ Basic_concepts/conditionals.py
@@ -0,0 +1,9 @@
+
+number = int(input("Ingresa un numero: "))
+
+if (( number > 5 and number < 10 ) or (number > 15 and number < 20)):
+  print( "( number > 5 and number < 10 ) or (number > 15 and number < 20)")
+elif ( number > 50):
+  print( " number > 50 ")
+else:
+  print( " else ")
