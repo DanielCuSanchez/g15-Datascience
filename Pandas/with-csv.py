90cf7e3d435f18545e7a578749ce944e7104c0ca
DanielCu <danielcu@alternet.com.mx>
2023-02-20 19:57:49 -0600

    with csv

---
+++ Pandas/with-csv.py
@@ -0,0 +1,13 @@
+import csv
+
+data = []
+with open("hurricanes.csv", "r") as file:
+  reader = csv.reader(file)
+  print(reader)
+  for row in reader:
+    print(row)
+    data.append(row)
+
+print(data)
+print("Header -------------", data[0])
+print("Content -------------",data[1:])
